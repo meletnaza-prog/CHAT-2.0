a4da805c6f7efc892fadec0628e7e0c65896a9e7
meletnaza-prog <meletnaza@gmail.com>
2026-01-29 16:58:30 -0400

    Add Flowise API endpoint for predictions
    Added a new Flowise API endpoint for predictions.

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ class AIChatApp {
         
         // ** (NEW) Flowise API Endpoint **
 this.flowiseEndpoint = "https://cloud.flowiseai.com/api/v1/prediction/3a44ea4c-5cc3-4496-bbd9-3d3eef81eec8";
+```
+
+7. **Scroll down to the bottom**
+
+8. **In "Commit message" write:**
+```
+   Update Flowise endpoint
         this.initializeEventListeners();
         this.setupAutoResize();
         this.loadChatHistory();
